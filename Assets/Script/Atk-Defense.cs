1e2191bb0df7a769539ee10c86942744509b882b
nad154 <119729778+nad154@users.noreply.github.com>
2025-03-11 14:33:12 +0100

    js

--- Assets/Script/Atk-Defense.cs
+++ Assets/Script/Atk-Defense.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Defense", menuName = "New Defense")]
 public class Defense : MoveSet{
     public override bool Execute(User P1, User P2, BaseMonster attacker, BaseMonster opponent){
-
+        return false; 
     }
 }
