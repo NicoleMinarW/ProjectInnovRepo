b4252a185777ce190c8979bb546d4c8091d3819d
NicoleMinarW <nicoleminar@yahoo.com>
2025-03-12 19:48:59 +0700

    Revert "dsada"
    This reverts commit 35244f730cff9d2261fda623f524e00d7b4a78a8.

--- Assets/Script/Sp-SpecialAttack.cs
+++ Assets/Script/Sp-SpecialAttack.cs
@@ -1,5 +1,5 @@
 using UnityEngine; 
 [CreateAssetMenu(fileName = "New Special Attack", menuName = "New Special Attack")]
 public abstract class SpecialAttack : ScriptableObject {
-    [SerializeField] private string _a;
+    [SerializeField] private string _a
 }
