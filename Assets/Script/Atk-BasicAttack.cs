5410d4ffbdbe3faf514d8e3bae36d5c01781f13c
nad154 <119729778+nad154@users.noreply.github.com>
2025-03-12 12:12:20 +0100

    def

--- Assets/Script/Atk-BasicAttack.cs
+++ Assets/Script/Atk-BasicAttack.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 public class BasicAttack : MoveSet 
 {
     [SerializeField] private float _damage; 
+    private float totalDamage; 
     public float Damage => _damage; 
     public override bool Execute(User P1, User P2, BaseMonster attacker, BaseMonster opponent){
         Debug.Log($"Executing {MoveName}: {attacker.data.monsterName} attacks {opponent.data.monsterName}");
         if(P1.costActionPoints(APCost)){
-            opponent._currHP -= (_damage + attacker._buff) - opponent._defense; 
+            totalDamage = _damage + attacker._buff; 
+            totalDamage -= totalDamage * (opponent._defense / 100);
+            opponent._currHP -= totalDamage;
+            opponent._currHP = Mathf.Clamp(opponent._currHP, 0, opponent.data.maxHP);
+
             Debug.Log($"Damage: {_damage}, Buff: {attacker._buff}, Opponent Defense: {opponent._defense}");
             Debug.Log($"Opponent's new HP: {opponent._currHP}");
             return CheckDeath(opponent); 
