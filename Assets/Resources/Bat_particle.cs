be573a1ee1f1791c5c5eb2f683b3efe20ee745a1
GhostofMalorn <550106@student.saxion.nl>
2025-03-12 19:10:49 +0100

    Added batamander

---
+++ Assets/Resources/Bat_particle.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Bat_particle : MonoBehaviour
+{
+    public SkinnedMeshRenderer skinnedMesh;  // The mesh with the blend shapes
+    public int vertexIndex;  // The index of the vertex to follow
+    public ParticleSystem particleEffect;  // The particle system to move
+
+    void LateUpdate()
+    {
+        if (skinnedMesh != null && particleEffect != null)
+        {
+            Mesh bakedMesh = new Mesh();
+            skinnedMesh.BakeMesh(bakedMesh); // Get the deformed mesh
+
+            // Convert the vertex position to world space
+            Vector3 worldVertexPos = skinnedMesh.transform.TransformPoint(bakedMesh.vertices[vertexIndex]);
+
+            // Move the particle effect to follow this vertex
+            particleEffect.transform.position = worldVertexPos;
+        }
+    }
+}
